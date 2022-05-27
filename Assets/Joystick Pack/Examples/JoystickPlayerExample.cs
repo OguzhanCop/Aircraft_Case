baba54bdde69030823f2855bb7070619bd2d5793
OguzhanCop <104174109+OguzhanCop@users.noreply.github.com>
2022-05-28 01:42:59 +0300

    v1

---
+++ Assets/Joystick Pack/Examples/JoystickPlayerExample.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickPlayerExample : MonoBehaviour
+{
+    public float speed;
+    public FixedJoystick variableJoystick;
+    public Rigidbody rb;
+
+    public void FixedUpdate()
+    {
+        Vector3 direction = Vector3.forward * variableJoystick.Horizontal + Vector3.right * variableJoystick.Vertical;
+        Vector3 force = Vector3.forward * variableJoystick.Vertical + Vector3.right * variableJoystick.Horizontal;
+        rb.AddRelativeForce(force.x * 10, 0,0);
+        rb.AddRelativeForce(0,0,2);
+        gameObject.transform.Rotate(-direction);
+    }
+    
+}
