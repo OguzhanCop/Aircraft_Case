e16750e1b75bc16f259e3cb731dda90681a3c4c7
Oğuzhan Cop <oguzhancop@gmail.com>
2022-06-01 14:03:42 +0300

    v7

--- Assets/PlaneRotationFollow.cs
+++ Assets/PlaneRotationFollow.cs
@@ -2,15 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Cinemachine;
+
 
 public class PlaneRotationFollow : MonoBehaviour
 {
+    
     public GameObject plane;
     public float planeInspectorX;
     public List<GameObject> target = new List<GameObject>();
     public static int i = 0;
     public TextMeshProUGUI leaderboard;
     private int score=0;
+    public  GameObject camera2;
+      public Transform yer;
 
     void Start()
     {
@@ -19,6 +24,8 @@ public class PlaneRotationFollow : MonoBehaviour
         
     void Update()
     {
+       
+
         leaderboard.text = "Score:" + score;
         planeInspectorX = plane.GetComponent<JoystickPlayerExample>().rotateZ;
         transform.rotation = Quaternion.Euler(90 - planeInspectorX*3, 90, -90);
@@ -35,16 +42,22 @@ public class PlaneRotationFollow : MonoBehaviour
             else
                 score -= 100;
             if (i < target.Count - 1)
-            
-            
+
+
             {
 
                 i++;
                 transform.position = target[i].transform.position;
             }
             else
-                Debug.Log("end");                
-              
+            {
+
+
+                plane.GetComponent<Animator>().SetBool("CameraSwitch", true);
+               
+                camera2.GetComponent<CinemachineVirtualCamera>().Follow = yer;
+
+            }
       
         }      
        
